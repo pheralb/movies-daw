eb6f918bc6befd0c467d48b2805d8178b23abe37
Jose Juan <josedec16@gmail.com>
2022-05-13 14:50:00 +0100

    ❌ Mejorada página de error

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import {HStack, Image, Text} from '@chakra-ui/react'
 const Error404 = () => {
   return (
     <>
       <Helmet>
         <title>Error 404</title>
       </Helmet>
-      <div>Error 404</div>
+      <HStack spacing={1} pr={3}>
+                <Image src="/images/logo_sin_texto.png" boxSize="250px" />
+                <Text fontSize="7xl">Error 404</Text>
+      </HStack>
+      <Text fontSize="3xl">Lo sentimos, pero esta página no está disponible</Text>
     </>
   )
 }
